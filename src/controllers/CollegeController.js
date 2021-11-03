f5d7455b24f1eefc962d098d18b6d65b3d5f3168
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-11-03 03:06:48 -0300

    Create controller to return college list names by year

---
+++ src/controllers/CollegeController.js
@@ -0,0 +1,80 @@
+const dbConnection = require('../database/connection');
+const Promise = require('bluebird');
+
+function orderByName(listObjects) {
+  return listObjects.sort((first, second) => {
+    if (first.no_curso > second.no_curso) {
+      return 1;
+    }
+    if (first.no_curso < second.no_curso) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+async function executeQuery(year) {
+  try {
+    return await dbConnection.raw(`select co_curso, no_curso from curso_${year}`);
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+function removeDuplicatesByName(listObjects) {
+  return listObjects.reduce((acc, cur, idx, src) => {
+    if (acc.length === 0) {
+      return cur;
+    };
+    const ids = new Set(acc.map(a => a.co_curso));
+    const merged = [...acc, ...cur.filter(b => !ids.has(b.co_curso))];
+    return merged;
+  }, []);
+};
+
+function getUniqueListBy(arr, key) {
+  return [...new Map(arr.map(item => [item[key], item])).values()]
+}
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const { range } = req.body.data;
+      let collegeList = [];
+      if (range.length > 1) {
+        const response = await Promise.map(
+          range,
+          async (year) => {
+            const names = await executeQuery(year);
+            return getUniqueListBy(names.rows, 'no_curso');
+          },
+          {concurrency: 2}
+        );
+
+        const responseUnduplicated = response.reduce((acc, cur, idx, src) => {
+          if (acc.length === 0) {
+            return cur;
+          };
+          const ids = new Set(acc.map(a => a.co_curso));
+          const id1 = new Set(src[0].map(a => a.co_curso));
+          const id2 = new Set(src[1].map(a => a.co_curso));
+          console.log(id1);
+          console.log(id2);
+          const merged = [...acc, ...cur.filter(b => !ids.has(b.co_curso))];
+          return merged;
+        }, []);
+        collegeList = responseUnduplicated;
+      }
+      else {
+        const year = range[0];
+        const response = await executeQuery(year);
+        const removedDuplicatedNames = getUniqueListBy(response.rows, 'no_curso');
+        const unDuplicatedNamesAndCode = getUniqueListBy(removedDuplicatedNames, 'co_curso');
+        collegeList = unDuplicatedNamesAndCode;
+      }
+      return res.json(orderByName(collegeList));
+    } catch (error) {
+      return res.json(error);
+    }
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { celebrate, Segments, Joi } = require("celebrate");
 const StudentController = require("./controllers/StudentController");
 const IesController = require("./controllers/IesController");
+const CollegeController = require("./controllers/CollegeController");
 
 const routes = express.Router();
 
 routes.get("/search", StudentController.index);
 routes.post("/ies_names", IesController.index);
+routes.post("/college_names", CollegeController.index);
 
 module.exports = routes;
