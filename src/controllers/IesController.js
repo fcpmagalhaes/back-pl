3f04f9df5a7f24bcbb719272d39e3bafa90663bb
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2022-08-18 03:50:20 -0300

    Finish ies controller

--- src/controllers/IesController.js
+++ src/controllers/IesController.js
@@ -3,19 +3,19 @@ const Promise = require('bluebird');
 
 function orderByName(listObjects) {
   return listObjects.sort((first, second) => {
-    if (first.no_ies > second.no_ies) {
+    if (first.label > second.label) {
       return 1;
     }
-    if (first.no_ies < second.no_ies) {
+    if (first.label < second.label) {
       return -1;
     }
     return 0;
   });
 };
 
-async function executeQuery(year) {
+async function genericQuery(query) {
   try {
-    return await dbConnection.raw(`select co_ies, no_ies from ies_${year}`);
+    return await dbConnection.raw(query);
   } catch(error) {
     console.log(error);
   }
@@ -24,13 +24,62 @@ async function executeQuery(year) {
 module.exports = {
   async index(req, res) {
     try {
+      const filters = [];
+      try {
+        const academicOrganizationQuery = `select * from organizacao_academica;`;
+        const academicOrganization = {};
+        const { rows } = await genericQuery(academicOrganizationQuery);
+
+        academicOrganization.value = 1;
+        academicOrganization.label = "Organização Acadêmica";
+        academicOrganization.type = "select";
+        academicOrganization.options = rows.map((row) => {
+          delete Object.assign(row, {value: row.id})['id'];
+          delete Object.assign(row, {label: row.descricao})['descricao'];
+          return row;
+        });
+        filters.push(academicOrganization);
+      } catch (error) {
+        return res.json(error);
+      }
+
+      try {
+        const adminCategoryQuery = `select * from categoria_administrativa;`;
+        const adminCategory = {};
+        const { rows } = await genericQuery(adminCategoryQuery);
+
+        adminCategory.value = 2;
+        adminCategory.label = "Categoria Administrativa";
+        adminCategory.type = "select";
+        adminCategory.options = rows.map((row) => {
+          delete Object.assign(row, {value: row.id})['id'];
+          delete Object.assign(row, {label: row.descricao})['descricao'];
+          return row;
+        });
+
+        filters.push(adminCategory);
+      } catch (error) {
+        return res.json(error);
+      }
+
+      return res.json(filters);
+    } catch (error) {
+      return res.json(error);
+    }
+  },
+
+  async iesNames(req, res) {
+    try {
       const { range } = req.body.data;
-      let iesList = [];
+      const filters = [];
+      let iesOptions = {};
+      let options = [];
+
       if (range.length > 1) {
         const response = await Promise.map(
           range,
           async (year) => {
-            const names = await executeQuery(year);
+            const names = await genericQuery(`select co_ies, no_ies from ies_${year};`);
             return names.rows;
           },
           {concurrency: 2}
@@ -44,15 +93,27 @@ module.exports = {
           const merged = [...acc, ...cur.filter(b => !ids.has(b.co_ies))];
           return merged;
         }, []);
-        iesList = responseUnduplicated;
+
+        options = responseUnduplicated;
       } 
       else {
         const year = range[0];
-        const response = await executeQuery(year);
-        iesList = response.rows;
+        const { rows } = await genericQuery(`select co_ies, no_ies from ies_${year};`);
+        options = rows;
       }
 
-      return res.json(orderByName(iesList));
+      iesOptions.value = 3;
+      iesOptions.label = "Nome da Instituição";
+      iesOptions.type = "select";
+      options = options.map((row) => {
+        delete Object.assign(row, {value: row.co_ies})['co_ies'];
+        delete Object.assign(row, {label: row.no_ies})['no_ies'];
+        return row;
+      });
+      iesOptions.options = orderByName(options);
+
+      filters.push(iesOptions);
+      return res.json(filters);
     } catch (error) {
       return res.json(error);
     }
