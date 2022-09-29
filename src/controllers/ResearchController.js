eec6d60676695e4aa72a1b1d1d2da5b20684b94e
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2022-09-29 02:37:08 -0300

    Finish configuration to get results of more one year

--- src/controllers/ResearchController.js
+++ src/controllers/ResearchController.js
@@ -9,209 +9,174 @@ async function executeQuery(year, whereFilters) {
   }
 };
 
+function subQuery(filter, column) {
+  const inSQL = 'IN';
+  const equalSQL = '=';
+  let optionsQuery = "";
+  if (filter.options.length === 1) {
+    optionsQuery = `${equalSQL} ${filter.options[0].value}`;
+  } else {
+    const options = filter.options.map(option => option.value);
+    optionsQuery = `${inSQL}(${options.join()})`;
+  }
+  return `${column} ${optionsQuery}`;
+};
+
+function subQueryCollegeName(filter, column, year) {
+  const inSQL = 'IN';
+ 
+  const options = filter.options.map(option => `'${option.label}'`);
+
+  const optionsQuery = `${inSQL}(${options.join()})`;
+
+  return `${column} ${inSQL}(select ${column} from curso_${year} where no_curso ${optionsQuery})`
+};
+
+function collegeFilterQuery(collegeFilters, year) {
+  if (collegeFilters.length !== 0 ) {
+    const andSQL = 'AND';
+    const query = collegeFilters.map((filter) => {
+
+      if (filter.value === 1) {
+        return subQuery(filter, 'TP_GRAU_ACADEMICO');
+      }            
+      if (filter.value === 2) {
+        return subQuery(filter, 'TP_TURNO');
+      };
+      if (filter.value === 3) {
+        return subQuery(filter, 'TP_NIVEL_ACADEMICO');
+      };
+      if (filter.value === 4) {
+        return subQuery(filter, 'TP_MODALIDADE_ENSINO');
+      };
+      if (filter.value === 5) {
+        return subQueryCollegeName(filter, 'CO_CURSO', year);
+      };
+      
+    });
+    return query.join(`${andSQL}`);
+  }
+  return null;
+};
+
+
 function iesFilterQuery(iesFilters) {
   if (iesFilters.length !== 0 ) {
     const andSQL = 'AND';
-    const inSQL = 'IN';
-    const equalSQL = '=';
-    const query = iesFilters.map((filter) => {
-      let queryFIlter ='';
-      if (filter.label === 'Nome da Instituição') {
-          let optionsCoIES;
-          if (filter.options.length === 1) {
-            optionsCoIES = `${equalSQL} ${filter.options[0].value}`;
-          } else {
-            optionsCoIES = `${inSQL}(`;
-            filter.options.map((option,index) => {
-              if (option === options[options.length -1]) {
-                optionsCoIES.concat(`${option.value})`);
-              } else {
-                optionsCoIES.concat(`${option.value},`);
-              }
-            });
-            console.log('optionsCoIES mais', optionsCoIES);
-          }
-          console.log('optionsCoIES', optionsCoIES);
-          console.log('queryFIlter antes', queryFIlter);
-          queryFIlter = `CO_IES ${optionsCoIES}`;
-          console.log('queryFIlter depois', queryFIlter);
-          return queryFIlter;
-
-      
-      } else if (filter.label === 'Organização Acadêmica') {
-        
-      } else if (filter.label === 'Categoria Administrativa') {
-        
+   
+    const query = iesFilters.map((filter) => {      
+      if (filter.value === 1) {
+        return subQuery(filter, 'TP_ORGANIZACAO_ACADEMICA');
       }
+      if (filter.value === 2) {
+        return subQuery(filter, 'TP_CATEGORIA_ADMINISTRATIVA');
+      };
+      if (filter.value === 3) {
+          return subQuery(filter, 'CO_IES');
+      };
     });
-    // CO_IES
-    // TP_CATEGORIA_ADMINISTRATIVA
-    // TP_ORGANIZACAO_ACADEMICA
-    console.log('QUERY', query);
-    // return query;
-    return ''
-
+    return query.join(` ${andSQL} `);
   }
-  return '';
-
-}
-
-function collegeFilterQuery(collegeFilters) {
-
-  query = '';
-  return query;
-  
-}
+  return null;
+};
 
 function studentFilterQuery(studentFilters) {
-
-  query = '';
-  return query;
-  
-}
+  if (studentFilters.length !== 0 ) {
+    const andSQL = 'AND';
+    const query = studentFilters.map((filter) => {
+      if (filter.value === 1) {
+        return subQuery(filter, 'TP_COR_RACA');
+      }
+      if (filter.value === 2) {
+        return subQuery(filter, 'TP_SEXO');
+      };
+      if (filter.value === 4) {
+        return subQuery(filter, 'TP_SITUACAO');
+      };
+      if (filter.value === 5) {
+        return subQuery(filter, 'IN_DEFICIENCIA');
+      };
+      if (filter.value === 27) {
+        return subQuery(filter, 'TP_ESCOLA_CONCLUSAO_ENS_MEDIO');
+      };
+      if (filter.value === 28) {
+        return subQuery(filter, 'TP_SEMESTRE_CONCLUSAO');
+      };
+      if (filter.value === 29) {
+        return subQuery(filter, 'IN_MOBILIDADE_ACADEMICA');
+      };
+      if (filter.value === 30) {
+        return subQuery(filter, 'TP_NACIONALIDADE');
+      };
+    });
+    return query.join(`${andSQL}`);
+  }
+  return null; 
+};
 
 module.exports = {
-  async testIndex(req, res) {
+  async index(req, res) {
     try {
       const { rangeYears, iesFilters, collegeFilters, studentFilters } = req.body.data;
       let finalResearch = [];
       if (rangeYears.length > 1) {
-        console.log("MAIS DE UM ANO");
+        // console.log("MAIS DE UM ANO");
         const iesOptions = iesFilterQuery(iesFilters);
-        const collegeOptions = iesFilterQuery(collegeFilters);
-        const studentOptions = iesFilterQuery(studentFilters);
+        const studentOptions = studentFilterQuery(studentFilters);
+        
+        
+        // console.log('iesOptions', iesOptions);
+        // console.log('studentOptions', studentOptions);
+        
+        const response = await Promise.map(
+          rangeYears,
+          async (year) => {
+            
+            const whereFilters = [];
+            console.log(year)
+            const collegeOptions = collegeFilterQuery(collegeFilters, year);
+            // console.log('collegeOptions', collegeOptions);
+
+            whereFilters.push(iesOptions, collegeOptions, studentOptions);
+            const whereFiltersNotNull = whereFilters.filter(filters => filters !== null).join(' AND ');
+            console.log('whereFiltersNotNull', whereFiltersNotNull);
+            
+            const data = await executeQuery(year, whereFiltersNotNull);
+            const payload = {
+              year,
+              finalCount: data.rows[0]
+            }
+            return payload;
+          },
+          {concurrency: 2}
+        );
+        
+        finalResearch = response;
+      } 
+      else {
+        console.log("UM ANO");
+        const iesOptions = iesFilterQuery(iesFilters);
+        // const collegeOptions = iesFilterQuery(collegeFilters);
+        // const studentOptions = iesFilterQuery(studentFilters);
 
         // console.log("iesOptions", iesOptions);
         // console.log("collegeOptions", collegeOptions);
         // console.log("studentOptions", studentOptions);
         
-        const whereFilters = `${iesOptions} ${collegeOptions} ${studentOptions}`
-
-        // const response = await Promise.map(
-        //   range,
-        //   async (year) => {
-        //     const data = await executeQuery(year, whereFilters);
-        //     console.log('data');
-        //     const payload = {
-        //       year,
-        //       finalCount: data.rows
-        //     }
-        //     return payload;
-        //   },
-        //   {concurrency: 2}
-        // );
+        // const whereFilters = `${iesOptions} ${collegeOptions} ${studentOptions}`;
+        // console.log('iesOptions', iesOptions);
+        const data = await executeQuery(rangeYears[0], iesOptions);
         
-        // finalResearch.push(response);
-      } 
-      else {
-        console.log("UM ANO");
-        const iesOptions = iesFilterQuery(iesFilters);
-        const collegeOptions = iesFilterQuery(collegeFilters);
-        const studentOptions = iesFilterQuery(studentFilters);
-
-        console.log("iesOptions", iesOptions);
-        console.log("collegeOptions", collegeOptions);
-        console.log("studentOptions", studentOptions);
-        
-        const whereFilters = `${iesOptions} ${collegeOptions} ${studentOptions}`;
-        const names = await executeQuery(year, whereFilters);
-        const data = await executeQuery(year, whereFilters);
-        console.log('data');
         const payload = {
-          year,
-          finalCount: data.rows
+          year: rangeYears[0],
+          finalCount: data.rows[0]
         }
         finalResearch.push(payload);
       }
-      return res.json(iesList);
+
+      return res.json(finalResearch);
     } catch (error) {
       return res.json(error);
     }
-  },
-
-  async index(req, res) {
-    try {
-      const { range } = req.body.data;
-      let iesList = [];
-      if (range.length > 1) {
-        const response = await Promise.map(
-          range,
-          async (year) => {
-            const names = await executeQuery(year);
-            return names.rows;
-          },
-          {concurrency: 2}
-        );
-
-        const responseUnduplicated = response.reduce((acc, cur, idx, src) => {
-          if (acc.length === 0) {
-            return cur;
-          };
-          const ids = new Set(acc.map(a => a.co_ies));
-          const merged = [...acc, ...cur.filter(b => !ids.has(b.co_ies))];
-          return merged;
-        }, []);
-        iesList = responseUnduplicated;
-      } 
-      else {
-        const year = range[0];
-        const response = await executeQuery(year);
-        iesList = response.rows;
-      }
-
-      return res.json(orderByName(iesList));
-    } catch (error) {
-      return res.json(error);
-    }
-  },
-
-  async oldIndex(req, res) {
-    try {
-
-      let searchRange = [];
-      const searchParams = req.body;
-
-      if (searchParams.range.length > 1) {
-        searchParams.range.map(async (year) => {
-          console.log('ano', year);
-          const result = await executeQuery(year).then((a) => searchRange.push(a));
-          console.log('result', result);
-          // searchRange.push(result);
-        }
-        );
-        console.log('searchRange', searchRange);
-        return res.json(searchRange);
-        // console.log('searchRange', searchRange);
-      
-      } else {
-        console.log('entrei2');
-        const result = await executeQuery(searchParams.range[0]);
-        searchRange.push(result);
-        return res.json(searchRange);
-        return res.json('all ok');
-      }
-      // console.log('searchRange', searchRange);
-      // return res.json(searchRange);
-
-    } catch (error){
-      return res.json(error);
-    }
-
-    async function executeQuery(year) {
-      try {
-        const resultado = await dbConnection.raw(`select count(*) from alunos_${year} where "tp_categoria_administrativa" = 1`);
-        console.log(resultado);
-        return resultado;
-      } catch(e) {
-        console.log(e);
-      }
-    }
-
   },
 };
-
-// SELECT count(*)
-// from public.alunos_df
-// INNER JOIN public.organizacao_academica
-// ON public.organizacao_academica.id = public.alunos_df.tp_organizacao_academica
-// WHERE public.organizacao_academica.tipo like('Universidade');
