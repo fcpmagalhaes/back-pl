6fc4f030adee901f70778a05eb3a318cbe49d29b
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-01-26 05:16:08 -0300

    Update port name

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.API_PORT || 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
