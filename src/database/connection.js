b14b6ee1732b514b8c57fd22f1dab6fd14137f74
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-01-30 02:01:18 -0300

    test connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,25 @@
-require('dotenv').config();
+// require('dotenv').config();
+
+// const knex = require("knex");
+// const configuration = require("../../knexfile");
+
+// const config = process.env.DB_ENVIRONMENT === 'production' ? configuration.production : configuration.development;
+// console.log('config', config);
+// module.exports = knex(config);
+
 
 const knex = require("knex");
-const configuration = require("../../knexfile");
+const parse = require("pg-connection-string").parse;
 
-const config = process.env.DB_ENVIRONMENT === 'production' ? configuration.production : configuration.development;
+// Parse the environment variable into an object containing User, Password, Host, Port etc at separate key-value pairs
+const pgconfig = parse(process.env.DATABASE_URL);
 
-module.exports = knex(config);
+// Add SSL setting to default environment variable on a new key-value pair (the value itself is an object)
+pgconfig.ssl = { rejectUnauthorized: false };
+
+const db = knex({
+  client: "pg",
+  connection: pgconfig,
+});
+
+module.exports = db;
